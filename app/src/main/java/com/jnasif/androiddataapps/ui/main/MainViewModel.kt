78858c25905320d84d218efafa1bcfb0338f3eb9
nasif-jaoadun <nasif_online@yahoo.com>
2025-02-25 23:51:59 +0600

    Parse Json Strings with Moshi

--- app/src/main/java/com/jnasif/androiddataapps/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jnasif/androiddataapps/ui/main/MainViewModel.kt
@@ -5,12 +5,26 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.jnasif.androiddataapps.LOG_TAG
 import com.jnasif.androiddataapps.R
+import com.jnasif.androiddataapps.data.Monster
 import com.jnasif.androiddataapps.utilities.FileHelper
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 
 class MainViewModel(app : Application) : AndroidViewModel(app) {
+    private val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
     init {
 //        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
         val text = FileHelper.getTextFromAsset(app, "monster_data.json")
-        Log.i(LOG_TAG, text)
+        parseText(text)
+    }
+
+    fun parseText(text : String){
+        val moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
+        val monsterData = adapter.fromJson(text)
+        for (monster in monsterData ?: emptyList()){
+            Log.i(LOG_TAG, "${monster.monsterName} (\$${monster.price})")
+        }
     }
 }
