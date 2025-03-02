cf898c55d618c1f24bb4f49154227170fe16e66f
nasif-jaoadun <nasif_online@yahoo.com>
2025-03-02 23:19:25 +0600

    Map Json and class property name

--- app/src/main/java/com/jnasif/androiddataapps/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jnasif/androiddataapps/ui/main/MainViewModel.kt
@@ -4,12 +4,12 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.jnasif.androiddataapps.LOG_TAG
-import com.jnasif.androiddataapps.R
 import com.jnasif.androiddataapps.data.Monster
 import com.jnasif.androiddataapps.utilities.FileHelper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainViewModel(app : Application) : AndroidViewModel(app) {
     private val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
@@ -20,11 +20,11 @@ class MainViewModel(app : Application) : AndroidViewModel(app) {
     }
 
     fun parseText(text : String){
-        val moshi = Moshi.Builder().build()
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
         val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
         val monsterData = adapter.fromJson(text)
         for (monster in monsterData ?: emptyList()){
-            Log.i(LOG_TAG, "${monster.monsterName} (\$${monster.price})")
+            Log.i(LOG_TAG, "${monster.name} (\$${monster.price})")
         }
     }
 }
