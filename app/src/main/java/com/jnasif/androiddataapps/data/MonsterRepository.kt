6ab5cd2499f969f04ae47d2cdf7381b0ce932f77
nasif-jaoadun <nasif_online@yahoo.com>
2025-03-03 02:03:48 +0600

    Acquire data in a repository class

---
+++ app/src/main/java/com/jnasif/androiddataapps/data/MonsterRepository.kt
@@ -0,0 +1,22 @@
+package com.jnasif.androiddataapps.data
+
+import android.content.Context
+import android.util.Log
+import com.jnasif.androiddataapps.LOG_TAG
+import com.jnasif.androiddataapps.utilities.FileHelper
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+class MonsterRepository {
+    private val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
+
+    fun getMonsterData(context : Context) : List<Monster>{
+//        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
+        val text = FileHelper.getTextFromAsset(context, "monster_data.json")
+        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
+        return adapter.fromJson(text) ?: emptyList()
+    }
+}
--- app/src/main/java/com/jnasif/androiddataapps/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jnasif/androiddataapps/ui/main/MainViewModel.kt
@@ -5,26 +5,20 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.jnasif.androiddataapps.LOG_TAG
 import com.jnasif.androiddataapps.data.Monster
+import com.jnasif.androiddataapps.data.MonsterRepository
 import com.jnasif.androiddataapps.utilities.FileHelper
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainViewModel(app : Application) : AndroidViewModel(app) {
-    private val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
+    private val dataRepo = MonsterRepository()
     init {
-//        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
-        val text = FileHelper.getTextFromAsset(app, "monster_data.json")
-        parseText(text)
-    }
-
-    fun parseText(text : String){
-        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
-        val monsterData = adapter.fromJson(text)
-        for (monster in monsterData ?: emptyList()){
+        val monsterData = dataRepo.getMonsterData(app)
+        for (monster in monsterData){
             Log.i(LOG_TAG, "${monster.name} (\$${monster.price})")
         }
     }
+
+
 }
