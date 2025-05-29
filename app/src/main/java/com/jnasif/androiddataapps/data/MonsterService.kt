e7f79edf42d57d73f856c59498d0244f8721de07
nasif-jaoadun <nasif_online@yahoo.com>
2025-05-29 19:03:18 +0600

    2.5 Define a Retrofit Interface

---
+++ app/src/main/java/com/jnasif/androiddataapps/data/MonsterService.kt
@@ -0,0 +1,9 @@
+package com.jnasif.androiddataapps.data
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MonsterService {
+    @GET("/feed/monster.json")
+    suspend fun getMonsterData(): Response<List<Monster>>
+}
