16e3b729935eaf4d870df59bcaa6b1c795c17835
nasif-jaoadun <nasif_online@yahoo.com>
2025-09-04 13:59:22 +0600

    5.1 Save files to internal storage

--- app/src/main/java/com/jnasif/androiddataapps/data/MonsterRepository.kt
+++ app/src/main/java/com/jnasif/androiddataapps/data/MonsterRepository.kt
@@ -8,6 +8,10 @@ import androidx.annotation.WorkerThread
 import androidx.lifecycle.MutableLiveData
 import com.jnasif.androiddataapps.LOG_TAG
 import com.jnasif.androiddataapps.WEB_SERVICE_URL
+import com.jnasif.androiddataapps.utilities.FileHelper
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -27,6 +31,7 @@ class MonsterRepository(val app: Application) {
             val service = retrofit.create(MonsterService::class.java)
             val serviceData = service.getMonsterData().body() ?: emptyList()
             monsterData.postValue(serviceData)
+            saveDataTOCache(serviceData)
         }
     }
 
@@ -42,5 +47,12 @@ class MonsterRepository(val app: Application) {
             callWebService()
         }
     }
-    
+
+    private fun saveDataTOCache(monsterData : List<Monster>){
+        val moshi = Moshi.Builder().build()
+        val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
+        val adapter: JsonAdapter<List<Monster>> = moshi.adapter(listType)
+        val json = adapter.toJson(monsterData)
+        FileHelper.saveTextToFile(app, json)
+    }
 }
